8ef133983f3babb4e353c98fb2f740bf58a8622b
Enis Kaner <enis.d.kaner@gmail.com>
2023-03-28 13:35:21 +0300

    Favourite Fragment added

---
+++ app/src/main/java/com/eniskaner/mcutrackers/ui/favourite/FavouriteFragment.kt
@@ -0,0 +1,29 @@
+package com.eniskaner.mcutrackers.ui.favourite
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.eniskaner.mcutrackers.R
+import com.eniskaner.mcutrackers.databinding.FragmentFavouriteBinding
+
+class FavouriteFragment : Fragment() {
+    private var _binding: FragmentFavouriteBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFavouriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
