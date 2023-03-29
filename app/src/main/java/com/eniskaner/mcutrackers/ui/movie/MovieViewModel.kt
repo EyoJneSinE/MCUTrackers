ff6e0fc18ae0a5ec117e97814aa6d5ae2501783f
Enis Kaner <enis.d.kaner@gmail.com>
2023-03-29 15:49:48 +0300

    MovieViewModel updated for Phase

--- app/src/main/java/com/eniskaner/mcutrackers/ui/movie/MovieViewModel.kt
+++ app/src/main/java/com/eniskaner/mcutrackers/ui/movie/MovieViewModel.kt
@@ -1,13 +1,29 @@
 package com.eniskaner.mcutrackers.ui.movie
 
+import androidx.annotation.MenuRes
 import androidx.lifecycle.ViewModel
 import com.eniskaner.mcutrackers.data.MovieRepository
+import com.eniskaner.mcutrackers.database.model.Phase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 @HiltViewModel
+@OptIn(ExperimentalCoroutinesApi::class)
 class MovieViewModel @Inject constructor(
     repository: MovieRepository
 ): ViewModel() {
-    val movies = repository.getMovies()
+    var phase = MutableStateFlow(Phase.ALL)
+        private set
+    val movies = phase.flatMapLatest { phase ->
+        when (phase) {
+            Phase.ALL -> repository.getMoviesByPhase(phase.ordinal)
+        }
+    }
+    fun setPhase(@MenuRes menuId: Int) {
+        Phase.map[menuId]?.let { phase.value = it }
+    }
 }
