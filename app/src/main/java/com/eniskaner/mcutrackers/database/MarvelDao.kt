f44d52ef6522679683010ae4c3bcab1998562cec
Enis Kaner <enis.d.kaner@gmail.com>
2023-03-29 12:23:06 +0300

    MarvelDao updated for insertMovies()

--- app/src/main/java/com/eniskaner/mcutrackers/database/MarvelDao.kt
+++ app/src/main/java/com/eniskaner/mcutrackers/database/MarvelDao.kt
@@ -1,12 +1,17 @@
 package com.eniskaner.mcutrackers.database
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.eniskaner.mcutrackers.database.model.Movie
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MarvelDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMovies(movies: List<Movie>)
+    
     @Query("SELECT*FROM Movie")
     fun getMovies(): Flow<List<Movie>>
 }
