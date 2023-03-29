6ca1c9a710a7b37e1dbd55def3cbfc89084a04a3
Enis Kaner <enis.d.kaner@gmail.com>
2023-03-29 15:42:21 +0300

    MovieRepository updated for getMoviesByPhase()

--- app/src/main/java/com/eniskaner/mcutrackers/data/MovieRepositoryImplementation.kt
+++ app/src/main/java/com/eniskaner/mcutrackers/data/MovieRepositoryImplementation.kt
@@ -14,4 +14,8 @@ class MovieRepositoryImplementation @Inject constructor(
     override fun getMovies(): Flow<List<Movie>> {
         return dao.getMovies().flowOn(ioDispatcher)
     }
+
+    override fun getMoviesByPhase(phase: Int): Flow<List<Movie>> {
+        return dao.getMoviesByPhase(phase).flowOn(ioDispatcher)
+    }
 }
