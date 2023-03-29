6c1849204dd744d4913491a23444d6906f34a50f
Enis Kaner <enis.d.kaner@gmail.com>
2023-03-29 12:14:48 +0300

    MarvelDao updated for getMovies()

--- app/src/main/java/com/eniskaner/mcutrackers/database/MarvelDao.kt
+++ app/src/main/java/com/eniskaner/mcutrackers/database/MarvelDao.kt
@@ -1,4 +1,12 @@
 package com.eniskaner.mcutrackers.database
 
+import androidx.room.Dao
+import androidx.room.Query
+import com.eniskaner.mcutrackers.database.model.Movie
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface MarvelDao {
+    @Query("SELECT*FROM Movie")
+    fun getMovies(): Flow<List<Movie>>
 }
