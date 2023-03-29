b03d7473ab1a95ceb6bb387b09dc8bebf563b01a
Enis Kaner <enis.d.kaner@gmail.com>
2023-03-29 14:22:10 +0300

    DataBaseModule updated for Pre-population the database

--- app/src/main/java/com/eniskaner/mcutrackers/di/DatabaseModule.kt
+++ app/src/main/java/com/eniskaner/mcutrackers/di/DatabaseModule.kt
@@ -2,13 +2,19 @@ package com.eniskaner.mcutrackers.di
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.eniskaner.mcutrackers.database.MarvelDao
 import com.eniskaner.mcutrackers.database.MarvelDatabase
+import com.eniskaner.mcutrackers.worker.MarvelDataBaseWorker
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import timber.log.Timber
 import javax.inject.Singleton
 
 @Module
@@ -21,6 +27,15 @@ object DatabaseModule {
             context,
             MarvelDatabase::class.java,
             "Marvel.db"
+        ).addCallback(
+            object : RoomDatabase.Callback() {
+                override fun onCreate(db: SupportSQLiteDatabase) {
+                    super.onCreate(db)
+                    val request = OneTimeWorkRequestBuilder<MarvelDataBaseWorker>().build()
+                    WorkManager.getInstance(context).enqueue(request)
+                    Timber.d("Database is initialized")
+                }
+            }
         ).build()
     }
     @Provides
