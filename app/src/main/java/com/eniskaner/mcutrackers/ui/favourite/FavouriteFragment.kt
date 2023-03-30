8d8c7c814aa736e4ce204adef12143338c5a965d
Enis Kaner <enis.d.kaner@gmail.com>
2023-03-30 16:37:23 +0300

    FavouriteFragment updated for initFavouritesCollect()

--- app/src/main/java/com/eniskaner/mcutrackers/ui/favourite/FavouriteFragment.kt
+++ app/src/main/java/com/eniskaner/mcutrackers/ui/favourite/FavouriteFragment.kt
@@ -5,12 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.eniskaner.mcutrackers.R
 import com.eniskaner.mcutrackers.databinding.FragmentFavouriteBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class FavouriteFragment : Fragment() {
     private var _binding: FragmentFavouriteBinding? = null
     private val binding get() = requireNotNull(_binding)
+    private val viewModel by viewModels<FavouriteViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +32,17 @@ class FavouriteFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRecyclerView()
+        initFavouritesCollect()
+    }
+
+    private fun initFavouritesCollect() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.favourites.collect { list ->
+                    (binding.rwFavourite.adapter as FavouriteAdapter).submitList(list)
+                }
+            }
+        }
     }
 
     private fun initRecyclerView() {
