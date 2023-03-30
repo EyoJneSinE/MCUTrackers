d16225b769546f503d85278be85312ec13d88fa9
Enis Kaner <enis.d.kaner@gmail.com>
2023-03-30 17:17:10 +0300

    MovieViewModel updated for StateFlow

--- app/src/main/java/com/eniskaner/mcutrackers/ui/movie/MovieViewModel.kt
+++ app/src/main/java/com/eniskaner/mcutrackers/ui/movie/MovieViewModel.kt
@@ -2,12 +2,12 @@ package com.eniskaner.mcutrackers.ui.movie
 
 import androidx.annotation.MenuRes
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.eniskaner.mcutrackers.data.repository.MovieRepository
 import com.eniskaner.mcutrackers.database.model.Phase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,18 +15,16 @@ import javax.inject.Inject
 class MovieViewModel @Inject constructor(
     repository: MovieRepository
 ): ViewModel() {
-    var phase = MutableStateFlow(Phase.ALL)
-        private set
-    val movies = phase.flatMapLatest { phase ->
+    private val _phase = MutableStateFlow(Phase.ALL)
+        val phase = _phase.asStateFlow()
+
+    val movies = _phase.flatMapLatest { phase ->
         when (phase) {
-            Phase.ALL -> repository.getMoviesByPhase(phase.ordinal)
-            Phase.ONE -> TODO()
-            Phase.TWO -> TODO()
-            Phase.THREE -> TODO()
-            Phase.FOUR -> TODO()
+            Phase.ALL -> repository.getMovies()
+            else -> repository.getMoviesByPhase(phase.ordinal)
         }
-    }
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
     fun setPhase(@MenuRes menuId: Int) {
-        Phase.map[menuId]?.let { phase.value = it }
+        Phase.map[menuId]?.let { _phase.value = it }
     }
 }
