51055560db5554e021db9d63293cb2ff62901311
Enis Kaner <enis.d.kaner@gmail.com>
2023-03-31 13:05:32 +0300

    MainActivity updated for BottomNavigationView visibility

--- app/src/main/java/com/eniskaner/mcutrackers/ui/main/MainActivity.kt
+++ app/src/main/java/com/eniskaner/mcutrackers/ui/main/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.eniskaner.mcutrackers.ui.main
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.view.WindowCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -28,6 +31,42 @@ class MainActivity : AppCompatActivity() {
     private fun getNavController(): NavController {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         return navHostFragment.navController
+    }
+
+    private fun addOnDestinationChangedListener(navController: NavController): NavController {
+        return navController.apply {
+            addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.movieFragment, R.id.favouriteFragment ->
+                        setBottomNavigationShowAnimation()
+                    else -> setBottomNavigationHideAnimation()
+                }
+            }
+        }
+    }
+    private fun setBottomNavigationShowAnimation() {
+        binding.bottomNavigation.takeIf { it.visibility == View.GONE }?.let {
+            it.animate().alpha(1f).setListener(
+                object : AnimatorListenerAdapter() {
+                    override fun onAnimationStart(animation: Animator) {
+                        super.onAnimationStart(animation)
+                        it.visibility = View.VISIBLE
+                    }
+                }
+            )
+        }
+    }
+    private fun setBottomNavigationHideAnimation() {
+        binding.bottomNavigation.takeIf { it.visibility == View.VISIBLE }?.let {
+            it.animate().alpha(0f).setListener(
+                object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        super.onAnimationEnd(animation)
+                        it.visibility = View.GONE
+                    }
+                }
+            )
+        }
     }
 
     private fun initDecorFitsSystemWindows() {
