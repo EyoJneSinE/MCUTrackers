7b28d347050a0aa3e95bdb3c3ff547f4711756d1
Enis Kaner <enis.d.kaner@gmail.com>
2023-04-03 15:16:54 +0300

    MovieViewModel updated for phase logic

--- app/src/main/java/com/eniskaner/mcutrackers/ui/movie/MovieViewModel.kt
+++ app/src/main/java/com/eniskaner/mcutrackers/ui/movie/MovieViewModel.kt
@@ -9,28 +9,25 @@ import com.eniskaner.mcutrackers.database.model.Phase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 @OptIn(ExperimentalCoroutinesApi::class)
 class MovieViewModel @Inject constructor(
     private val repository: MovieRepository,
-    private val savedStateHandle: SavedStateHandle
 ): ViewModel() {
-    private val key = "Phase"
-    private val _phase = MutableStateFlow(savedStateHandle[key] ?: Phase.ALL)
-        val phase = _phase.asStateFlow()
+    val phase = repository.getPhase().stateIn(viewModelScope, SharingStarted.WhileSubscribed(), null )
 
-    val movies = _phase.flatMapLatest { phase ->
+    val movies = phase.filterNotNull().flatMapLatest { phase ->
         when (phase) {
             Phase.ALL -> repository.getMovies()
             else -> repository.getMoviesByPhase(phase.ordinal)
         }
     }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
     fun setPhase(@MenuRes menuId: Int) {
-        Phase.map[menuId]?.let {
-            _phase.value = it
-            savedStateHandle[key] = it
+        viewModelScope.launch {
+            repository.setPhase(menuId)
         }
     }
 }
